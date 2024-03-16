ce62c7af0b2e543d63329b7879d0cfe38cef40b5
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-16 09:49:03 +0300

    ...

---
+++ AuthanticationWebApp/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthanticationWebApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public string? Family { get; set; }
+    }
+}
