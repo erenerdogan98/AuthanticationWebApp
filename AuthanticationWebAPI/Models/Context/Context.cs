7e7ea58a4175f028a9d9948cb4410ac325a37ffc
erenerdogan98 <eren.erdogannn@hotmail.com>
2024-03-18 02:13:08 +0300

    added model folder and entities (tables) , added Context class for database operations , and added migration then update database(MySql confugration is appsettingjson - program.cs)

---
+++ AuthanticationWebAPI/Models/Context/Context.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthanticationWebAPI.Models.Context
+{
+    public class Context(DbContextOptions options) : IdentityDbContext(options)
+    {
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
